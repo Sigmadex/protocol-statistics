5b2796d45f840661b6e2c8e7a2366014a5762c34
modium <rtql8d@gmail.com>
2023-02-21 17:27:09 -0700

    creates local JSON data for testing purposes

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react'
+
+const dashboardData = {
+  totalSDEXStaked: 62500,
+  totalCirculatingNFTs: 44,
+  totalAPYNFTsMinted: 10400,
+  nftAPYMinters: 101,
+  totalAPYNFTsBeingFarmed: 109,
+  SDEXBurnedFromPenalties: 45900,
+  maximumInflation: 594,
+  lpTokensForfeited: 5500000
+}
+
 export function Dashboard() {
+  const [data, setDashboardData] = useState({})
+
+  useEffect(() => {
+    setDashboardData(dashboardData)
+  }, []) // use walletAddress as parameter
+
   return (
     <div>
       <h1>Dashboard</h1>
       <p>View a global overview of the protocol statistics.</p>
       <hr />
+      {Object.entries(data).length &&
+        data['totalSDEXStaked']}
     </div>
   )
 }
--- src/components/Positions/index.js
+++ src/components/Positions/index.js
@@ -1,5 +1,62 @@
-export function Positions() {
+import { useState, useEffect } from 'react'
+
+const positionData = {
+  mining: [
+    {
+      tokenName: 'SDEX',
+      activeDuration: 720,
+      maturity: 82,
+      unitsStaked: 45000,
+      nftAPY: 112
+    },
+    {
+      tokenName: 'SDEX',
+      activeDuration: 365,
+      maturity: 0, // if maturity === 0, token has matured
+      unitsStaked: 10000,
+      nftAPY: 42
+    },
+    {
+      tokenName: 'SDEX LP',
+      activeDuration: 365,
+      maturity: 4,
+      unitsStaked: 55, // spec says "55 JLP"?
+      nftAPY: 200
+    }    
+  ],
+  earning: [
+    {
+      tokenName: 'SDEX',
+      activeDuration: 720,
+      maturity: 82,
+      unitsStaked: 45000,
+      earned: 5000,
+      apy: 112
+    }
+  ]
+}
+
+const StakingModal = () => {
   return (
-    <p>View your active stakes.</p>
+    <div>
+      <h1>Create New Staking Position</h1>
+    </div>
+  )
+}
+
+export function Positions() {
+  const [data, setDashboardData] = useState({})
+
+  useEffect(() => {
+    setDashboardData(positionData)
+  }, []) // use walletAddress as parameter
+
+  return (
+    <div>
+      <p>View your active stakes.</p>
+      {Object.entries(data).length &&
+        data['earning'][0]['apy']}
+      <StakingModal />
+    </div>
   )
 }
