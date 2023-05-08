70188375dba8275d3a8872c0c76cbb60490c4f6b
Jaf Crisologo <rtql8d@gmail.com>
2023-05-08 16:08:50 -0600

    sets up boilerplate for token search and filtering

--- src/components/SelectToken/index.js
+++ src/components/SelectToken/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-const localCoinData = require('../local_coin_data.json')
+const localCoinData = require("../local_coin_data.json");
 
 const tempTokens = [
   {
@@ -14,36 +14,52 @@ const tempTokens = [
   },
 ];
 
-const Search = ({filterTokens}) => {
+const Search = ({ filterTokens }) => {
   return (
-    <input onChange={e => filterTokens(e.target.value)} className="form-control" tyoe='text' />
-  )
-}
+    <input
+      onChange={(e) => filterTokens(e.target.value)}
+      className="form-control"
+      tyoe="text"
+    />
+  );
+};
 
-const MostPopular = ({tokens}) => {
+const MostPopular = ({ tokens }) => {
   return (
     <div>
-      {tokens.map(token => 
-        <div style={{backgroundColor: 'gold', fontSize: 14, fontWeight: 'bold', display: 'inline-block', width: 83, height: 44, border: '1px solid #404C55', borderRadius: 10, marginRight: 10}}>
+      {tokens.map((token) => (
+        <div
+          style={{
+            backgroundColor: "gold",
+            fontSize: 14,
+            fontWeight: "bold",
+            display: "inline-block",
+            width: 83,
+            height: 44,
+            border: "1px solid #404C55",
+            borderRadius: 10,
+            marginRight: 10,
+          }}
+        >
           <img
             alt={`token.name`}
-            src='/images/avax-logo.svg'
-            style={{width: 24, height: 24}}
+            src="/images/avax-logo.svg"
+            style={{ width: 24, height: 24 }}
           />
           {token.asset_id}
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export const SelectToken = () => {
   const [selectedToken, setSelectedToken] = useState("");
   const [tokens, setTokens] = useState([]);
 
-  const apiKey = process.env.REACT_APP_COINAPI_KEY
-  const assetsEndpoint = "https://rest.coinapi.io/v1/assets/"
-  const limit = '10' // limit coins to 10
+  const apiKey = process.env.REACT_APP_COINAPI_KEY;
+  const assetsEndpoint = "https://rest.coinapi.io/v1/assets/";
+  const limit = "10"; // limit coins to 10
 
   // useEffect(() => {
   //   fetch(`https://rest.coinapi.io/v1/assets/?apiKey=${apiKey}&limit=${limit}`, {
@@ -57,8 +73,8 @@ export const SelectToken = () => {
   // }, [])
 
   useEffect(() => {
-    setTokens(localCoinData)
-  }, [])
+    setTokens(localCoinData);
+  }, []);
 
   // useEffect(() => {
   //   if (tokens.length) {
@@ -67,11 +83,15 @@ export const SelectToken = () => {
   // }, [tokens])
 
   function filterTokens(filterBy) {
-    if (filterBy !== '') { // || tokens.length > 0
-      console.log('filtering by ', filterBy)
-      let filteredTokens = [...tokens]
-        .filter(token => token.asset_id.toLowerCase() === filterBy.toLowerCase() || token.name.toLowerCase() === filterBy.toLowerCase())
-      setTokens(filteredTokens)
+    if (filterBy !== "") {
+      // || tokens.length > 0
+      console.log("filtering by ", filterBy);
+      let filteredTokens = [...tokens].filter(
+        (token) =>
+          token.asset_id.toLowerCase() === filterBy.toLowerCase() ||
+          token.name.toLowerCase() === filterBy.toLowerCase()
+      );
+      setTokens(filteredTokens);
     }
   }
 
@@ -81,32 +101,28 @@ export const SelectToken = () => {
       <Search filterTokens={filterTokens} />
       <MostPopular tokens={tokens} />
       <div>
-        <ul style={{padding: 0, listStyleType: 'none'}}>
+        <ul style={{ padding: 0, listStyleType: "none" }}>
           {/* {tokens.length ? tokens.map(token => <li key={token.tokenSymbol}>{token.tokenName}</li>) : null} */}
-          {tokens.length && 
-            tokens.map(token => {
+          {tokens.length &&
+            tokens.map((token) => {
               return (
                 <li key={token.asset_id}>
-                  <div style={{display: 'inline-block'}}>
+                  <div style={{ display: "inline-block" }}>
                     <img
                       alt={`token.name`}
-                      src='/images/avax-logo.svg'
-                      style={{display: 'inline-block', width: 42, height: 42}}
+                      src="/images/avax-logo.svg"
+                      style={{ display: "inline-block", width: 42, height: 42 }}
                     />
                     {/* {`${token.asset_id} - $${token.price_usd.toFixed(3)}`} */}
 
-                    <div className='row'>
-                      {token.name}
-                    </div>
-                    <div className='row'>
-                      {token.asset_id}
-                    </div>
+                    <div className="row">{token.name}</div>
+                    <div className="row">{token.asset_id}</div>
                   </div>
-                  <div style={{display: 'inline-block'}}>
+                  <div style={{ display: "inline-block" }}>
                     ${token.price_usd.toFixed(3)}
                   </div>
                 </li>
-              )
+              );
             })}
         </ul>
       </div>
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,5 +1,55 @@
+import { useState, useEffect } from "react";
+
+const localCoinData = require("./local_coin_data.json");
+
+const Search = ({ setSearched }) => {
+  return (
+    <input
+      type="text"
+      onChange={(e) => setSearched(e.target.value)}
+      placeholder="Search for token"
+    />
+  );
+};
+
+const MostPopular = ({ tokens }) => {
+  return (
+    <div>
+      {tokens.map((token, i) => (
+        <span key={i}>{token.asset_id}</span>
+      ))}
+    </div>
+  );
+};
+
+const filteredTokens = (tokens, searched) => {
+  return searched === ""
+    ? tokens
+    : tokens.filter(
+        (token) =>
+          token.asset_id.toLowerCase().startsWith(searched.toLowerCase()) ||
+          token.name.toLowerCase().startsWith(searched.toLowerCase())
+      );
+};
+
 export function Test() {
-    return (
-        <h1>Test</h1>
-    )
+  const [tokens, setTokens] = useState([]);
+  const [searched, setSearched] = useState("");
+
+  useEffect(() => {
+    setTokens(localCoinData);
+  }, []);
+
+  return (
+    <div>
+      <h1>Test</h1>
+      <Search setSearched={setSearched} />
+      <MostPopular tokens={filteredTokens(tokens, searched)} />
+      <ul>
+        {filteredTokens(tokens, searched).map((token, i) => (
+          <li key={i}>{token.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
