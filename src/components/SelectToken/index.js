647e5f7720e0d9361b151185350f2398341acb5d
modium <rtql8d@gmail.com>
2023-05-09 18:19:05 -0600

    cleans up styling for SelectToken component, adds ability to select token

--- src/components/SelectToken/index.js
+++ src/components/SelectToken/index.js
@@ -13,7 +13,7 @@ const Search = ({ setSearched }) => {
   );
 };
 
-const MostPopular = ({ tokens }) => (
+const MostPopular = ({ tokens, selectToken }) => (
   <div
     style={{
       marginBottom: 20,
@@ -24,7 +24,11 @@ const MostPopular = ({ tokens }) => (
     }}
   >
     {tokens.map((token, i) => (
-      <div style={{ display: "inline-block" }}>
+      <div
+        style={{ display: "inline-block" }}
+        key={i}
+        onClick={() => selectToken(token.asset_id)}
+      >
         <div
           style={{
             fontSize: 14,
@@ -55,19 +59,10 @@ const MostPopular = ({ tokens }) => (
   </div>
 );
 
-const filteredTokens = (tokens, searched) => {
-  return searched === ""
-    ? tokens
-    : tokens.filter(
-        (token) =>
-          token.asset_id.toLowerCase().startsWith(searched.toLowerCase()) ||
-          token.name.toLowerCase().startsWith(searched.toLowerCase())
-      );
-};
-
 export const SelectToken = () => {
   const [tokens, setTokens] = useState([]);
   const [searched, setSearched] = useState("");
+  const [selectedToken, setSelectedToken] = useState(null);
 
   const apiKey = process.env.REACT_APP_COINAPI_KEY;
   const assetsEndpoint = "https://rest.coinapi.io/v1/assets/";
@@ -88,8 +83,24 @@ export const SelectToken = () => {
     setTokens(localCoinData);
   }, []);
 
+  const filteredTokens = (tokens, searched) => {
+    return searched === ""
+      ? tokens
+      : tokens.filter(
+          (token) =>
+            token.asset_id.toLowerCase().startsWith(searched.toLowerCase()) ||
+            token.name.toLowerCase().startsWith(searched.toLowerCase())
+        );
+  };
+
+  const selectToken = (token) => {
+    // alert("Selected ", token);
+    console.log("Selected", token);
+    // setSelectedToken(token)
+  };
+
   return (
-    <div className="col-lg-6 offset-lg-3">
+    <div className="col-lg-4 offset-lg-4">
       <div
         className="card"
         style={{
@@ -99,31 +110,50 @@ export const SelectToken = () => {
           marginTop: 40,
           // padding: "30px 20px",
           padding: "35px",
+          // height: 598,
         }}
       >
         <h1 style={{ fontSize: 34 }}>Select a token</h1>
         <Search setSearched={setSearched} />
-        <MostPopular tokens={filteredTokens(tokens, searched)} />
+        <MostPopular
+          tokens={filteredTokens(tokens, searched)}
+          selectToken={selectToken}
+        />
         {filteredTokens(tokens, searched).map((token, i) => {
           return (
             <div
+              // className="p-3"
               style={{
-                borderBottom: "1px solid rgba(64, 76, 85, 0.5)",
+                borderBottom: "1px solid rgba(64, 76, 85, 0.2)",
                 height: 68,
                 display: "flex",
                 justifyContent: "space-between",
+                alignItems: "center",
               }}
               key={i}
+              onClick={() => selectToken(token.asset_id)}
             >
-              <span>
+              <div style={{ display: "inline-block" }}>
                 <img
                   alt={`${token.name}`}
                   src={`/images/eth-logo.svg`}
-                  style={{ width: 42, height: 42 }}
+                  style={{
+                    width: 42,
+                    height: 42,
+                    float: "left",
+                    marginRight: 10,
+                    paddingTop: 5,
+                  }}
                 />
-                {token.name}
-              </span>
-              <span>{token.price_usd.toFixed(3)}</span>
+                <div style={{ float: "right" }}>
+                  <div>
+                    <strong>{token.name}</strong>
+                  </div>
+                  <div>{token.asset_id}</div>
+                </div>
+              </div>
+
+              <div>{token.price_usd.toFixed(2)}</div>
             </div>
           );
         })}
